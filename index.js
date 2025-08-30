3db8d323f09c791889989ae5fc8e4d482779c590
playmaker65 <167650295+playmaker65@users.noreply.github.com>
2025-08-30 15:31:26 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+//Js: Timers: setInterval, requestAnimationFrame(libry)
+
+// JQuery: Dom Maninpulation: to easily select elements, change text
+           //Event Handling
+           //Animations
+
+$(document).ready(function(){
+    alert("Document and is ready and linked to Jquery");
+}) 
